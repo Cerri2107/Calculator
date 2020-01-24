4fc6e97bef1527d54a27683c29ad10708f5e910b
Cerri2107 <cerri2107@gmail.com>
2020-01-25 00:17:48 +0100

    added logarithm (only base 10 and e) functions, removed the useless deck class, changed a few comments and some code to make it more readable

--- calculator.cpp
+++ calculator.cpp
@@ -5,28 +5,6 @@
 #include <string>
 
 namespace cerri {
-	//deque wrapper
-	template<typename Type = float>
-	class deck {
-		std::deque<Type> deq;
-	public:
-		//return memorized result of a set age (how many tokens ago it was saved)
-		Type peek(const size_t& age) {
-			return deq[deq.size() - age];
-		}
-		void push_back(const Type& value) {
-			deq.push_back(value);
-		}
-		Type pop_front() {
-			Type out = deq.front();
-			deq.pop_front();
-			return out;
-		}
-		size_t size() {
-			return deq.size();
-		}
-	};
-
 	//calculator class
 	class calculator {
 		//can keep the result of any operation, and its validity
@@ -35,24 +13,28 @@ namespace cerri {
 			bool valid = true;
 		};
 
+		//defined string deque as a type for brevity
 		typedef std::deque<std::string> str_deque;
-		deck<float> memory;							//keeps every old result
-		str_deque tokens;							//keeps every "word" of an expression
-		str_deque ops_pr_1 { "*", "/", "^", "v" };	//operators to execute first
-		str_deque ops_pr_2 { "+", "-" };			//operators to execute second
-		str_deque::iterator exec_point;				//keeps track of which operator is being executed
+		std::deque<float> memory;								//keeps every old result
+		str_deque tokens;								//keeps every "word" of an expression
+		str_deque ops_pr_1 { "^", "v", "log", "ln" };	//operators to execute first
+		str_deque ops_pr_2{ "*", "/" };					//operators to execute second
+		str_deque ops_pr_3 { "+", "-" };				//operators to execute last
+		str_deque::iterator exec_point;					//keeps track of which operator is being executed
 
-		/*write a function that finds the last parenthesis divided snippet*/
+		//returns a pair containing begin and end iterators of the last parenthesis divided snippet in the tokens deque*/
 		std::pair<str_deque::iterator, str_deque::iterator> getnextsnippet() {
 			str_deque::iterator begin, end;
 			str_deque::reverse_iterator rbegin;
+
 			rbegin = std::find(tokens.rbegin(), tokens.rend(), "(");
 			if (rbegin != tokens.rend())
 				rbegin++;
 			begin = rbegin.base();
-			end = std::find(begin, tokens.end(), ")");
 			if (begin == tokens.end())
 				begin = tokens.begin();
+			end = std::find(begin, tokens.end(), ")");
+
 			return std::pair<str_deque::iterator, str_deque::iterator>(begin, end);
 		}
 		//returns a string deque containing the tokens of the expression
@@ -78,9 +60,14 @@ namespace cerri {
 			std::pair<str_deque::iterator, str_deque::iterator> snip = getnextsnippet();
 			str_deque::iterator snip_begin = snip.first;
 			str_deque::iterator snip_end = snip.second;
+
 			out = std::find_first_of(snip_begin, snip_end, ops_pr_1.begin(), ops_pr_1.end());
-			if (out == snip_end)
+			if (out == snip_end) {
 				out = std::find_first_of(snip_begin, snip_end, ops_pr_2.begin(), ops_pr_2.end());
+				if (out == snip_end)
+					out = std::find_first_of(snip_begin, snip_end, ops_pr_3.begin(), ops_pr_3.end());
+			}
+
 			size_t out_i = std::distance(tokens.begin(), out);
 			size_t end_i = std::distance(tokens.begin(), snip_end);
 			if (snip_end - snip_begin < 5) {
@@ -91,24 +78,25 @@ namespace cerri {
 				}
 				if (tokens.begin() + end_i != tokens.end())
 					tokens.erase(tokens.begin() + end_i);
+				out = tokens.begin() + out_i;
 			}
-			out = tokens.begin() + out_i;
+
 			return out;
 		}
 		//recursively converts an "operand" (which includes r_{num}, sqrt_{num}, etc...) to real numbers kept in the struct out_t
 		out_t strtoop(const std::string& operand) {
 			out_t output;
-			std::string prefix = operand.substr(0, operand.find_first_of('_'));
-			std::string suffix = operand.substr(operand.find_first_of('_') + 1);
+			std::string prefix = operand.substr(0, operand.find_first_of('('));
+			std::string suffix = operand.substr(prefix.length(), operand.find_last_of(')'));
 
 			//checks the prefix for all the preset ones
 
 			#pragma region if "r"
 			if (prefix == "r") {						//r_{num} returns number of num age from memory
 				if (output.valid = isunsint(suffix)) {
-					int age = stoi(suffix);
+					size_t age = stoi(suffix);
 					if (output.valid = age < memory.size() + 1)
-						output.value = memory.peek(age);
+						output.value = memory[memory.size() - age];
 				}
 				return output;
 			}
@@ -130,6 +118,24 @@ namespace cerri {
 					output.value = num.value * num.value;
 				return output;
 			}
+			#pragma endregion
+
+			#pragma region if "log"
+
+
+
+			#pragma endregion
+
+			#pragma region if "ln"
+
+
+
+			#pragma endregion
+
+			#pragma region if a constant
+
+
+
 			#pragma endregion
 
 			#pragma region if a number
@@ -161,42 +167,63 @@ namespace cerri {
 				out.value = powf(o_left.value, out.value);
 			else if (op == "v")
 				out.value = powf(out.value, 1 / o_left.value);
+			else if (op == "log")
+				out.value = log10f(out.value);
+			else if (op == "ln")
+				out.value = logf(out.value);
 			else {
 				out.valid = false;
 				return out;
 			}
 			#pragma endregion
 
-			*(exec_point - 1) = std::to_string(out.value);
-			tokens.erase(exec_point, exec_point + 2);
-			exec_point = nextop();
-			if (exec_point != tokens.end())
+			if (op == "log" || op == "ln") {
+				if (exec_point != tokens.begin() && !isoperator(*(exec_point - 1)))
+					tokens.insert(exec_point - 1, "*");
+				*(exec_point) = std::to_string(out.value);
+				tokens.erase(exec_point + 1, exec_point + 2);
+				exec_point = nextop();
+			}
+			else {
+				*(exec_point - 1) = std::to_string(out.value);
+				tokens.erase(exec_point, exec_point + 2);
+				exec_point = nextop();
+			}
+
+			if (exec_point == tokens.end())
+				return out;
+			else if (*exec_point != "log" && *exec_point != "ln")
 				return execop(strtoop(*(exec_point - 1)), *exec_point, *(exec_point + 1));
 			else
-				return out;
+				return execop(out_t(), *exec_point, *(exec_point + 1));			
 		}
 
 	public:
 		//starts the execution of an expression
 		out_t startex(const std::string& exp) {
 			out_t out;
-			tokens = dividetokens(exp);
 			
-			if (out.valid = tokens.size() > 2 && tokens.size() % 2 == 1) {
+			try {
+				tokens = dividetokens(exp);
 				exec_point = nextop();
-				if (out.valid = exec_point != tokens.begin() && exec_point != tokens.end()) {
-					try {
+				if (exec_point == tokens.end())
+					out = strtoop(*(exec_point - 1));
+				else if (exec_point != tokens.end() - 1)
+					if (*exec_point == "log" || *exec_point == "ln")
+						out = execop(out_t(), *exec_point, *(exec_point + 1));
+					else
 						out = execop(strtoop(*(exec_point - 1)), *exec_point, *(exec_point + 1));
-					}
-					catch (...) {
-						out.valid = false;
-					}
-					if (out.valid)
-						memory.push_back(out.value);
-				}
+				else
+					out.valid = false;
+			}
+			catch (...) {
+				out.valid = false;
 			}
 
+			if (out.valid)
+				memory.push_back(out.value);
 			tokens.erase(tokens.begin(), tokens.end());
+
 			return out;
 		}
 		//return whether a string contains a valid signed float number
@@ -214,9 +241,22 @@ namespace cerri {
 		//return whether a string contains a valid unsigned integer number
 		bool isunsint(const std::string& s) {
 			for (int i = 0; i < s.length(); i++)
-				if (!isdigit(s[i]) /*&& i == 0 && s[i] != '-' && s[i] != '+'*/)
+				if (!isdigit(s[i]))
 					return false;
 			return true;
+		}
+		//return whether a string is a valid operator
+		bool isoperator(const std::string& s) {
+			for (int i = 0; i < ops_pr_1.size(); i++)
+				if (s == ops_pr_1.at(i))
+					return true;
+			for (int i = 0; i < ops_pr_2.size(); i++)
+				if (s == ops_pr_2.at(i))
+					return true;
+			for (int i = 0; i < ops_pr_3.size(); i++)
+				if (s == ops_pr_3.at(i))
+					return true;
+			return false;
 		}
 
 		typedef out_t out_type;
